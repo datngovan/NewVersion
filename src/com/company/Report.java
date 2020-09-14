e27d0c500f7d444e38940d9d2a2f74cc98ab9a29
ngovandat <s3817813@rmit.edu.vn>
2020-09-14 16:42:23 +0700

    master

--- src/com/company/Report.java
+++ src/com/company/Report.java
@@ -3,6 +3,8 @@ package com.company;
 import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
@@ -20,43 +22,49 @@ public class Report implements interfacesOfReport {
         long count10_20 = 0;
         long count20_60 = 0;
         long count60 = 0;
-        Date currentTime = new Date();
-        long currentSeconds = currentTime.getTime()/1000;
-        try{
+        LocalDate now = LocalDate.now();
+        try {
             Scanner fileReading = new Scanner(new File("lead.csv"));
-            while(fileReading.hasNext()){
+            while (fileReading.hasNext()) {
                 String eachLine = fileReading.nextLine();
-                if(eachLine.equals(leadTitle)){
+                if (eachLine.equals(leadTitle)) {
 
-                }
-                else {
+                } else {
                     String dob = eachLine.split(",")[2];
                     SimpleDateFormat getFormat = new SimpleDateFormat("yyyy-MM-dd");
                     Date date = getFormat.parse(dob);
-                    long age = (currentSeconds-(date.getTime()/1000))/(24*60*60*365);
-                    if(age>=0 && age<10){
-                        count0_10 +=1;
-                    }else if(age>=10 && age <20){
-                        count10_20+=1;
-                    }else if(age>=20 && age <60){
-                        count20_60+=1;
-                    }else{
-                        count60+=1;
+                    Calendar c = Calendar.getInstance();
+                    c.setTime(date);
+                    int year = c.get(Calendar.YEAR);
+                    int month = c.get(Calendar.MONTH) + 1;
+                    int day = c.get(Calendar.DATE);
+                    LocalDate birthDate = LocalDate.of(year, month, day);
+                    Period diff = Period.between(birthDate, now);
+                    int age = diff.getYears();
+                    System.out.println(age);
+                    if (age >= 0 && age < 10) {
+                        count0_10 += 1;
+                    } else if (age >= 10 && age < 20) {
+                        count10_20 += 1;
+                    } else if (age >= 20 && age < 60) {
+                        count20_60 += 1;
+                    } else {
+                        count60 += 1;
                     }
 
                 }
 
             }
             fileReading.close();
-            System.out.printf("%20s","0-10  (years old)");
-            System.out.printf("%20s","10-20  (years old)");
-            System.out.printf("%20s","20-60  (years old)");
-            System.out.printf("%20s%n",">60  (years old)");
-            System.out.printf("%20s",count0_10);
-            System.out.printf("%20s",count10_20);
-            System.out.printf("%20s",count20_60);
-            System.out.printf("%20s%n",count60);
-        }catch (Exception exception) {
+            System.out.printf("%20s", "0-10  (years old)");
+            System.out.printf("%20s", "10-20  (years old)");
+            System.out.printf("%20s", "20-60  (years old)");
+            System.out.printf("%20s%n", ">60  (years old)");
+            System.out.printf("%20s", count0_10);
+            System.out.printf("%20s", count10_20);
+            System.out.printf("%20s", count20_60);
+            System.out.printf("%20s%n", count60);
+        } catch (Exception exception) {
 
         }
     }
--- src/com/company/testFunction.java
+++ src/com/company/testFunction.java
@@ -9,87 +9,65 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
+import java.time.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class testFunction {
+
     public static void main(String[] args) {
-        String leadTitle = "stringCode,name,birthDate,gender,phone,email,address";
         String interactionTitle = "codeString,date,stringCodeOfLead,mean,status";
-        checkFile checker = new checkFile();
-
-        boolean leadFileExist = checker.getBoolean("lead.csv");
-        int lengthOfLeadArray = checker.getLength("lead.csv");
-
-        Lead[] listOfLeads = new Lead[lengthOfLeadArray - 1];
-
-        int leadIndex = 0;
-
+        String leadTitle = "stringCode,name,birthDate,gender,phone,email,address";
+        long count0_10 = 0;
+        long count10_20 = 0;
+        long count20_60 = 0;
+        long count60 = 0;
+        LocalDate now = LocalDate.now();
         try{
             Scanner fileReading = new Scanner(new File("lead.csv"));
-            while (fileReading.hasNext()){
+            while(fileReading.hasNext()){
                 String eachLine = fileReading.nextLine();
                 if(eachLine.equals(leadTitle)){
 
-                }else{
-                    String[] theArray = eachLine.split(",");
-                    SimpleDateFormat simpleDateformat = new SimpleDateFormat("yyyy-MM-dd");
-                    Date date = simpleDateformat.parse(theArray[2]);
-                    if (theArray[3].equals("true")){
-                        Lead lead = new Lead(theArray[0],theArray[1],date,true,theArray[4],theArray[5],theArray[6]);
-                        listOfLeads[leadIndex] = lead;
-                    }
-                    else {
-                        Lead lead = new Lead(theArray[0],theArray[1],date,false,theArray[4],theArray[5],theArray[6]);
-                        listOfLeads[leadIndex] = lead;
-                    }
-
-                    leadIndex += 1;
                 }
-            }
-            fileReading.close();
-        }catch (Exception exception){
-
-        }
-
-        boolean interactionListExist = checker.getBoolean("interaction.csv");
-        int lengthOfInteractionArray = checker.getLength("interaction.csv");
-        Interaction[] listOfInteraction = new Interaction[lengthOfInteractionArray - 1];
-        System.out.println(lengthOfInteractionArray);
-        int interactionIndex = 0;
-        try{
-            Scanner fileReading = new Scanner(new File("interaction.csv"));
-            while (fileReading.hasNext()){
-                String eachLine = fileReading.nextLine();
-                if(eachLine.equals(interactionTitle)){
-
-                }
-                else{
-                    String[] theArray = eachLine.split(",");
-                    SimpleDateFormat simpleDateformat = new SimpleDateFormat("yyyy-MM-dd");
-                    Date date = simpleDateformat.parse(theArray[1]);
-                    for (Lead lead: listOfLeads){
-                        if (lead.getCodeString().equals(theArray[2])){
-                            Interaction interaction = new Interaction(theArray[0],date,lead,theArray[3],theArray[4]);
-                            listOfInteraction[interactionIndex] = interaction;
-                        }
+                else {
+                    String dob = eachLine.split(",")[2];
+                    SimpleDateFormat getFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    Date date = getFormat.parse(dob);
+                    Calendar c = Calendar.getInstance();
+                    c.setTime(date);
+                    int year = c.get(Calendar.YEAR);
+                    int month = c.get(Calendar.MONTH) + 1;
+                    int day = c.get(Calendar.DATE);
+                    LocalDate birthDate = LocalDate.of(year,month,day);
+                    Period diff = Period.between(birthDate,now);
+                    int age = diff.getYears();
+                    System.out.println(age);
+                    if(age>=0 && age<10){
+                        count0_10 +=1;
+                    }else if(age>=10 && age <20){
+                        count10_20+=1;
+                    }else if(age>=20 && age <60){
+                        count20_60+=1;
+                    }else{
+                        count60+=1;
                     }
 
-                    interactionIndex += 1;
                 }
 
             }
             fileReading.close();
-        }catch (Exception exception){
+            System.out.printf("%20s","0-10  (years old)");
+            System.out.printf("%20s","10-20  (years old)");
+            System.out.printf("%20s","20-60  (years old)");
+            System.out.printf("%20s%n",">60  (years old)");
+            System.out.printf("%20s",count0_10);
+            System.out.printf("%20s",count10_20);
+            System.out.printf("%20s",count20_60);
+            System.out.printf("%20s%n",count60);
+        }catch (Exception exception) {
 
         }
-        String testCase = "lead_001";
-        try{
-            for(Interaction interaction: listOfInteraction){
-                System.out.println(interaction.getStringCode());
-            }
-        }catch (Exception e){
 
-        }
     }
 }
