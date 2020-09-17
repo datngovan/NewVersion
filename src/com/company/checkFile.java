25df09c6a40d1a92e8aaccfdaf87abea6e3da7fa
ngovandat <s3817813@rmit.edu.vn>
2020-09-17 13:24:46 +0700

    add

--- src/com/company/checkFile.java
+++ src/com/company/checkFile.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class checkFile {
+    /** This method return a boolean value. it will return true if the file is existing or false otherwise.*/
     public boolean getBoolean(String pathName){
         int numberOfLines = 0;
         boolean check = false;
@@ -28,6 +29,7 @@ public class checkFile {
         }
         return check;
     }
+    /** This method return the number of lines in the file.*/
     public int getLength(String pathName){
         int numberOfLines = 0;
         boolean check = false;
